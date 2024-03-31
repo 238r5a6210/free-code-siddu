74119a1039bb9a7414eb373b119b75a7f1369269
siddhartha <lasts@siddhartha_M>
2024-03-31 22:07:24 +0530

    add hash and hash compare function

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, hash) => {
+      console.log(res);
+  });
+});
 
 //END_ASYNC
 
